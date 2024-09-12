312d93338f344f1442c21b9ad3f5ef3d7f629c77
Guilherme Daniel <gui.dani13@gmail.com>
2024-09-12 00:00:05 -0300

    pagina inicial

---
+++ src/app/beginner/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header>
+        <h1>Beginner Challenges</h1>
+      </header>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <p>By: Guilherme Daniel</p>
+      </footer>
+    </div>
+  );
+}
