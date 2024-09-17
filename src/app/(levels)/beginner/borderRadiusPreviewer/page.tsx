8795df50ccf9fd116c454b0927c904b0ca70f0b5
Guilherme Daniel <gui.dani13@gmail.com>
2024-09-17 15:34:50 -0300

    update function

--- src/app/(levels)/beginner/borderRadiusPreviewer/page.tsx
+++ src/app/(levels)/beginner/borderRadiusPreviewer/page.tsx
@@ -4,10 +4,10 @@ import { useReducer } from "react";
 const rangeReducer = (state: Array<number>, action: { type: string, index: number, value: number }) => {
   switch (action.type) {
     case 'SET_VALUE':
-      // Retorna um novo estado com o valor atualizado no índice específico
-      return state.map((value: number, index: number) =>
-        index === action.index ? action.value : value
-      );
+      return {
+        ...state,
+        [action.key]: action.value,
+      };
     default:
       return state;
   }
@@ -15,27 +15,67 @@ const rangeReducer = (state: Array<number>, action: { type: string, index: numbe
 
 export default function Page() {
 
-  const [rangeValues, dispatch] = useReducer(rangeReducer, [0, 0, 0, 0]);
+  // const [rangeValues, dispatch] = useReducer(rangeReducer, [0, 0, 0, 0]);
+  // Inicializa o reducer com um objeto contendo 4 valores
+  const [rangeValues, dispatch] = useReducer(rangeReducer, {
+    top_right: 0,
+    top_left: 0,
+    bottom_right: 0,
+    bottom_left: 0,
+  });
 
-  const handleInputChange = (index: number, value: number) => {
-    dispatch({ type: 'SET_VALUE', index, value });
+  const handleInputChange = (key, value) => {
+    dispatch({ type: 'SET_VALUE', key, value });
   };
   return (
     <main className="flex flex-col items-center bg-slate-700 text-white">
       <h1 className="text-4xl font-bold p-4">Border Radius Previewer</h1>
       <div>
-        {rangeValues.map((value: number, index: number) => (
-          <div key={index}>
-            <input
-              type="range"
-              min="0"
-              max="100"
-              value={value}
-              onChange={(e) => handleInputChange(index, Number(e.target.value))}
-            />
-            <span>Valor: {value}</span>
-          </div>
-        ))}
+        <label>Top Right:</label>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={rangeValues.top_right}
+          onChange={(e) => handleInputChange('top_right', Number(e.target.value))}
+        />
+        <span>Valor: {rangeValues.top_right}</span>
+      </div>
+
+      <div>
+        <label>Top Left:</label>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={rangeValues.top_left}
+          onChange={(e) => handleInputChange('top_left', Number(e.target.value))}
+        />
+        <span>Valor: {rangeValues.top_left}</span>
+      </div>
+
+      <div>
+        <label>Bottom Right:</label>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={rangeValues.bottom_right}
+          onChange={(e) => handleInputChange('bottom_right', Number(e.target.value))}
+        />
+        <span>Valor: {rangeValues.bottom_right}</span>
+      </div>
+
+      <div>
+        <label>Bottom Left:</label>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={rangeValues.bottom_left}
+          onChange={(e) => handleInputChange('bottom_left', Number(e.target.value))}
+        />
+        <span>Valor: {rangeValues.bottom_left}</span>
       </div>
 
       <div className="square w-64 h-64 border-white border-2">
