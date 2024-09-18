441331ab058a5554a07d99273bff5774d57f39ec
Guilherme Daniel <gui.dani13@gmail.com>
2024-09-18 14:40:08 -0300

    design calculator

--- src/app/(levels)/beginner/calculator/Calculator.tsx
+++ src/app/(levels)/beginner/calculator/Calculator.tsx
@@ -1,29 +1,39 @@
+"use client"
+
+import { useState } from "react";
+
 export default function Calculator() {
+  const [calculo, setCalculo] = useState<string>("");
+
+  function handler(e: HTMLButtonElement) {
+    setCalculo(prev => prev + e.innerHTML);
+  }
+
   return (
     <div className="grid grid-cols-4 grid-rows-6 gap-1">
-      <div className="display row-span-1 col-span-4 border rounded-sm border-white flex justify-end items-end p-1">
-        DISPLAY
+      <div className="display row-span-1 col-span-4 border rounded-md border-white flex justify-end items-end p-1">
+        {calculo}
       </div>
-      <button className="p-4 border rounded-sm">CA</button>
-      <button className="p-4 border rounded-sm">C</button>
-      <button className="p-4 border rounded-sm">DEL</button>
-      <button className="p-4 border rounded-sm">/</button>
-      <button className="p-4 border rounded-sm">7</button>
-      <button className="p-4 border rounded-sm">8</button>
-      <button className="p-4 border rounded-sm">9</button>
-      <button className="p-4 border rounded-sm">x</button>
-      <button className="p-4 border rounded-sm">4</button>
-      <button className="p-4 border rounded-sm">5</button>
-      <button className="p-4 border rounded-sm">6</button>
-      <button className="p-4 border rounded-sm">-</button>
-      <button className="p-4 border rounded-sm">1</button>
-      <button className="p-4 border rounded-sm">2</button>
-      <button className="p-4 border rounded-sm">3</button>
-      <button className="p-4 border rounded-sm">+</button>
-      <button className="p-4 border rounded-sm">+/-</button>
-      <button className="p-4 border rounded-sm">0</button>
-      <button className="p-4 border rounded-sm">,</button>
-      <button className="p-4 border rounded-sm">=</button>
+      <button className="p-4 border rounded-md">CA</button>
+      <button className="p-4 border rounded-md">C</button>
+      <button className="p-4 border rounded-md">DEL</button>
+      <button className="p-4 border rounded-md">/</button>
+      <button className="p-4 border rounded-md">7</button>
+      <button className="p-4 border rounded-md">8</button>
+      <button className="p-4 border rounded-md">9</button>
+      <button className="p-4 border rounded-md">x</button>
+      <button className="p-4 border rounded-md">4</button>
+      <button className="p-4 border rounded-md">5</button>
+      <button className="p-4 border rounded-md">6</button>
+      <button className="p-4 border rounded-md">-</button>
+      <button className="p-4 border rounded-md">1</button>
+      <button className="p-4 border rounded-md">2</button>
+      <button className="p-4 border rounded-md">3</button>
+      <button className="p-4 border rounded-md">+</button>
+      <button className="p-4 border rounded-md">+/-</button>
+      <button className="p-4 border rounded-md" onClick={() => handler}>0</button>
+      <button className="p-4 border rounded-md">,</button>
+      <button className="p-4 border rounded-md">=</button>
     </div>
   )
 }
